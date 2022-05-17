aa4726e6617a1da5db6bdd2807632867c69f287b
hayashinoshun <s.y1014torimaru@gmail.com>
2022-05-18 00:20:51 +0900

    form作成中

--- SNS/public/message_add.php
+++ SNS/public/message_add.php
@@ -6,7 +6,21 @@ try {
     $message_text = $_POST['text'];
     $message_image = $_FILES['image'];
     $user_id = $_SESSION['user_id'];
-    $destination_user_id
+    $destination_user_id = $_POST['destination_user_id'];
+
+    if($message_text == '') {
+        set_flash('danger', 'メッセージ内容が未入力です');
+        reload();
+    }
+
+    if($message_image['size'] > 0) {
+        if($message_image['size'] > 1000000) {
+            set_flash('danger', '画像が大きすぎます');
+            reload();
+        } else {
+            move_uploaded_file($message_image['tmp_name'], './image/'.$message_image['name']);
+        }
+    }
 }
 
 ?>
