27f2c3901fe236bc82276f7f665447dbad8c8172
hayashinoshun <s.y1014torimaru@gmail.com>
2022-05-18 23:40:48 +0900

    お問合せformのcsrf対策

--- form_file/form.php
+++ form_file/form.php
@@ -1,4 +1,8 @@
-
+<?php 
+//セッション入れて上げて送信時に確認するため
+session_start();
+require_once '../function.php' 
+?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -15,6 +19,7 @@
         <p><input type="text" name="email"></p>
         <p>お問い合わせ内容</p>
         <p><textarea name="form"></textarea></p>
+        <input type="hidden" name="csrf_token" value="<?php echo h(setToken()); ?>">
         <input type="submit" value="送信">
     </form>
 </body>
--- form_file/form_return.php
+++ form_file/form_return.php
@@ -1,12 +1,23 @@
 <?php
+// //お問合せ内容をメールで受け取る
+session_start();
 
-$posts = $_POST;
-
-//formから送られたものを不当なscriptが発動しないように表示させる。
-foreach($posts as $key => $value) {
-    echo $key.' : '.htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . "<br>";
-    echo '<br>';
+//トークンが入ってるかまたpostで送られてきたトークンと一致するか？正規ルートで入ってこないとエラー出す！
+$token = filter_input(INPUT_POST, 'csrf_token');
+if(!isset($_SESSION['csrf_token']) || $token !== $_SESSION['csrf_token']) {
+    exit('不正なリクエストです。');
 }
+unset($_SESSION['csrf_token']);//残す必要ないためメモリ解放とセキュリティ面でも消す
 ?>
-main3
-main2
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問合せ確認</title>
+</head>
+<body>
+    
+</body>
+</html>
